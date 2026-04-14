a5c040a1501fe4a6d52e2ebdf13a3edbb5709d37
Ayush-ui309 <ayushrktsingh2006@gnmail.com>
2026-04-15 00:31:47 +0530

    uc7

--- src/main/java/helloapp.java
+++ src/main/java/helloapp.java
@@ -1,16 +1,6 @@
 public class Helloapp {
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
-        } else {
-            StringBuilder names = new StringBuilder();
-
-            for (String name : args) {
-                names.append(name).append(", ");
-            }
-
-            String result = names.substring(0, names.length() - 2);
-            System.out.println("Hello, " + result + "!");
-        }
+        String names = (args.length == 0) ? "World" : String.join(", ", args);
+        System.out.println("Hello, " + names + "!");
     }
 }
